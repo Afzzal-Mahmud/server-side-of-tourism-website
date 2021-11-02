304c65a66c6eb94ac8d9a0d7644c81811a1b0188
afzal mahmud <afzzalmahmud@gmail.com>
2021-11-01 20:19:58 -0700

    load data from local server

---
+++ index.js
@@ -0,0 +1,52 @@
+const { MongoClient } = require('mongodb')
+/* sequre password by dotenv package */
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 5000
+
+/* use middlewere */
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@node-express-crud.gezbh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/',(req,res) =>{
+    res.send('welcome to the server side')
+})
+
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db("adventureTurisom");
+        const offersCollection = database.collection("offerCollectionList")
+
+        /* sending offerData to server */
+        app.post('/offers',async (req,res) =>{
+            const offers = req.body
+            console.log('hitting the post',offers)
+            const result = await offersCollection.insertOne(offers)
+            console.log(result)
+            res.json(result)
+        })
+
+        /* getting application data from mongoDb database */
+        app.get('/alloffers',async(req,res) =>{
+            const cursor = offersCollection.find({})
+            const allOffers = await cursor.toArray()
+            res.send(allOffers)
+        })
+        console.log('connecting to database')
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+app.listen(port,() =>{
+    console.log('listening to the port',port)
+})
